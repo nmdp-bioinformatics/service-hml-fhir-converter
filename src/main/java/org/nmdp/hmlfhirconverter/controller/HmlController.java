3c07202dd49900d7b23a38db7c4d97bc45f02e9a
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2016-12-21 16:00:01 -0600

    Stub out API controller.

--- src/main/java/org/nmdp/hmlfhirconverter/controller/HmlController.java
+++ src/main/java/org/nmdp/hmlfhirconverter/controller/HmlController.java
@@ -5,8 +5,20 @@ package org.nmdp.hmlfhirconverter.controller;
  */
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/hml")
+@Api(value = "HmlFhirConverter", description= = "Hml to Fhir Converter Service")
 public class HmlController {
-
+    
 }
