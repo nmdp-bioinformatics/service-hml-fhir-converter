cc0a3820a021ea5ea846e157600bdf7bb098b565
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2016-12-22 10:37:04 -0600

    Stub out data layer.

--- src/main/java/org/nmdp/hmlfhirconverter/domain/TypingTestName.java
+++ src/main/java/org/nmdp/hmlfhirconverter/domain/TypingTestName.java
@@ -4,8 +4,65 @@ package org.nmdp.hmlfhirconverter.domain;
  * Created by abrown3 on 12/21/16.
  */
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 
-public class TypingTestName {
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+public class TypingTestName implements Serializable {
 
+    @XmlAttribute
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @XmlAttribute
+    private String name;
+
+    @XmlAttribute
+    private String description;
+
+    public TypingTestName() {
+
+    }
+
+    public TypingTestName(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public TypingTestName(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "TypingTestName {" +
+                " id = " + id +
+                " name = " + name +
+                " description = " + description +
+                " }";
+    }
 }
---
+++ src/main/java/org/nmdp/hmlfhirconverter/service/HmlService.java
@@ -0,0 +1,64 @@
+package org.nmdp.hmlfhirconverter.service;
+
+/**
+ * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 12/22/16.
+ * <p>
+ * service-hmlFhirConverter
+ * Copyright (c) 2012-2016 National Marrow Donor Program (NMDP)
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;  if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+ * <p>
+ * > http://www.fsf.org/licensing/licenses/lgpl.html
+ * > http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+import org.nmdp.hmlfhirconverter.domain.TypingTestName;
+import org.nmdp.hmlfhirconverter.dao.IHmlRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HmlService implements IHmlService {
+    private final IHmlRepository hmlRepository;
+
+    @Autowired
+    public HmlService(@Qualifier("hmlRepository") IHmlRepository hmlRepository) {
+        this.hmlRepository = hmlRepository;
+    }
+
+    @Override
+    public TypingTestName getTypingTestName(String id) {
+        return hmlRepository.findOne(id);
+    }
+
+    @Override
+    public TypingTestName createTypingTestName(String name, String description) {
+        return hmlRepository.save(new TypingTestName(name, description));
+    }
+
+    public TypingTestName updateTypingTestName(TypingTestName typingTestName) {
+        return new TypingTestName();
+    }
+
+    public TypingTestName deleteTypingTestName(String id) {
+        return new TypingTestName();
+    }
+
+    public TypingTestName deleteTypingTestName(TypingTestName typingTestName) {
+        return new TypingTestName();
+    }
+}
