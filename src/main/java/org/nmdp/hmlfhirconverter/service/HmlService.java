a8a69961c47fa9a3676d8003cf1a6c1374bf493b
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2016-12-27 12:09:40 -0600

    Fix build errors.

--- src/main/java/org/nmdp/hmlfhirconverter/service/HmlService.java
+++ src/main/java/org/nmdp/hmlfhirconverter/service/HmlService.java
@@ -29,12 +29,9 @@ import org.nmdp.hmlfhirconverter.dao.IHmlRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.ArrayList;
 
 @Service
 public class HmlService implements IHmlService {
@@ -51,9 +48,8 @@ public class HmlService implements IHmlService {
     }
 
     @Override
-    public List<TypingTestName> getTypingTestNames(Integer size) {
-        Pageable maxQuery = new PageRequest(0, size);
-        return hmlRepository.getTypingTestNames(maxQuery);
+    public List<TypingTestName> getTypingTestNames(Integer maxResults) {
+        return hmlRepository.getTypingTestNames(maxResults);
     }
 
     @Override
