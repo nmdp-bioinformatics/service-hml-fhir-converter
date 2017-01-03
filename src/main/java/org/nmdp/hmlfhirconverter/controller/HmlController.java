119d22fcdf8ccc7bca9a1ffa43ff15a28957f512
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-01-03 16:01:58 -0600

    Error handling and Swagger IO Integration.

--- src/main/java/org/nmdp/hmlfhirconverter/controller/HmlController.java
+++ src/main/java/org/nmdp/hmlfhirconverter/controller/HmlController.java
@@ -10,12 +10,16 @@ import org.nmdp.hmlfhirconverter.util.Converters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.api.NotFoundException;
 import io.swagger.model.TypingTestName;
 import io.swagger.api.HmlApi;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -30,10 +34,15 @@ public class HmlController implements HmlApi {
     }
 
     @Override
-    public Callable<ResponseEntity<List<TypingTestName>>> getTypingTestNames(Integer maxResults) throws NotFoundException {
-        List<org.nmdp.hmlfhirconverter.domain.TypingTestName> result = hmlService.findByMaxReturn(maxResults).getContent();
-        List<TypingTestName> transferResult = Converters.convertList(result, r -> r.toDto());
-        return () -> new ResponseEntity<>(transferResult, HttpStatus.OK);
+    @RequestMapping(value = "typingTestNames/{maxResults}", produces = "application/json", method = RequestMethod.GET)
+    public Callable<ResponseEntity<List<TypingTestName>>> getTypingTestNames(@PathVariable Integer maxResults) throws NotFoundException {
+        try {
+            List<org.nmdp.hmlfhirconverter.domain.TypingTestName> result = hmlService.findByMaxReturn(maxResults).getContent();
+            List<TypingTestName> transferResult = Converters.convertList(result, r -> r.toDto());
+            return () -> new ResponseEntity<>(transferResult, HttpStatus.OK);
+        } catch (Exception ex) {
+            return () -> new ResponseEntity<>(new ArrayList<TypingTestName>(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
---
+++ src/main/java/org/nmdp/hmlfhirconverter/handlers/HmlControllerHandler.java
@@ -0,0 +1,44 @@
+package org.nmdp.hmlfhirconverter.handlers;
+
+/**
+ * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 1/3/17.
+ * <p>
+ * service-hmlFhirConverter
+ * Copyright (c) 2012-2017 National Marrow Donor Program (NMDP)
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;  if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+ * <p>
+ * > http://www.fsf.org/licensing/licenses/lgpl.html
+ * > http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+import org.nmdp.hmlfhirconverter.exception.HmlControllerException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.apache.log4j.Logger;
+
+@ControllerAdvice
+public class HmlControllerHandler {
+    private final static Logger LOG = Logger.getLogger(HmlControllerHandler.class);
+
+    @ExceptionHandler(HmlControllerException.class)
+    public ResponseEntity handlerException(HmlControllerException ex) {
+        LOG.error(ex.getMessage(), ex);
+        return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
+    }
+}
