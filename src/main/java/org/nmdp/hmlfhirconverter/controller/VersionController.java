8452724c1148e8e34fa11e0b22a91083d97c3015
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-01-23 17:50:51 -0600

    Added version objectification REST support.

---
+++ src/main/java/org/nmdp/hmlfhirconverter/controller/VersionController.java
@@ -0,0 +1,159 @@
+package org.nmdp.hmlfhirconverter.controller;
+
+/**
+ * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 1/23/17.
+ * <p>
+ * service-hmlFhirConverter
+ * Copyright (c) 2012-2017 National Marrow Donor Program (NMDP)
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;  if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+ * <p>
+ * > http://www.fsf.org/licensing/licenses/lgpl.html
+ * > http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+import io.swagger.model.TypeaheadQuery;
+import org.nmdp.hmlfhirconverter.service.VersionService;
+import org.nmdp.hmlfhirconverter.util.Converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import io.swagger.api.NotFoundException;
+import io.swagger.model.Version;
+import io.swagger.api.VersionApi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+
+@RestController
+@RequestMapping("/version")
+@CrossOrigin
+public class VersionController implements VersionApi {
+    private final VersionService versionService;
+    private static final Logger LOG = Logger.getLogger(VersionController.class);
+
+    @Autowired
+    public VersionController(VersionService versionService) {
+        this.versionService = versionService;
+    }
+
+    @Override
+    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public Callable<ResponseEntity<List<Version>>> createVersions(@RequestBody List<Version> versions) throws NotFoundException {
+        try {
+            List<org.nmdp.hmlfhirconverter.domain.Version> result = versionService.createVersions(versions);
+            List<Version> transferResult = Converters.convertList(result, r -> r.toDto(r));
+            return () -> new ResponseEntity<>(transferResult, HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /createMulti", ex);
+            return () -> new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
+    public Callable<ResponseEntity<Boolean>> deleteVersion(@RequestBody Version version) throws NotFoundException {
+        try {
+            return () -> new ResponseEntity<>(versionService.deleteVersion(version), HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /delete", ex);
+            return () -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Callable<ResponseEntity<Boolean>> deleteVersion(@PathVariable String id) throws NotFoundException {
+        try {
+            return () -> new ResponseEntity<>(versionService.deleteVersion(id), HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /delete/{id}", ex);
+            return () -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(path = "/{maxResults}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public Callable<ResponseEntity<List<Version>>> getTypeaheadVersions(@PathVariable(value = "maxResults") Integer maxResults, @RequestBody TypeaheadQuery typeaheadQuery) throws NotFoundException {
+        try {
+            List<org.nmdp.hmlfhirconverter.domain.Version> result = versionService.getTypeaheadVersions(maxResults, typeaheadQuery);
+            List<Version> transferResult = Converters.convertList(result, r -> r.toDto(r));
+            return () -> new ResponseEntity<>(transferResult, HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /{maxResults}", ex);
+            return () -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
+    public Callable<ResponseEntity<Version>> getVersion(@PathVariable String id) throws NotFoundException {
+        try {
+            org.nmdp.hmlfhirconverter.domain.Version version = versionService.getVersion(id);
+            return () -> new ResponseEntity<>(version.toDto(version), HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /get/{id}", ex);
+            return () -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(path = "/{maxResults}/{pageNumber}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Callable<ResponseEntity<List<Version>>> getVersions(@PathVariable Integer maxResults, @PathVariable Integer pageNumber) throws NotFoundException {
+        try {
+            List<org.nmdp.hmlfhirconverter.domain.Version> result = versionService.findVersionsByMaxReturn(maxResults, pageNumber).getContent();
+            List<Version> transferResult = Converters.convertList(result, r -> r.toDto(r));
+            return () -> new ResponseEntity<>(transferResult, HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /getMulti/{maxResults}", ex);
+            return () -> new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Callable<ResponseEntity<Version>> getModel() {
+        try {
+            return () -> new ResponseEntity<>(new Version(), HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error getting model.", ex);
+            return () -> new ResponseEntity<>(new Version(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
+    public Callable<ResponseEntity<Version>> updateVersion(@RequestBody Version version) throws NotFoundException {
+        try {
+            org.nmdp.hmlfhirconverter.domain.Version nmdpModel = versionService.updateVersion(version);
+            return () -> new ResponseEntity<>(nmdpModel.toDto(nmdpModel), HttpStatus.OK);
+        } catch (Exception ex) {
+            LOG.error("Error on /update", ex);
+            return () -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/org/nmdp/hmlfhirconverter/service/VersionServiceImpl.java
@@ -0,0 +1,114 @@
+package org.nmdp.hmlfhirconverter.service;
+
+/**
+ * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 1/23/17.
+ * <p>
+ * service-hmlFhirConverter
+ * Copyright (c) 2012-2017 National Marrow Donor Program (NMDP)
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;  if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+ * <p>
+ * > http://www.fsf.org/licensing/licenses/lgpl.html
+ * > http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+import io.swagger.model.TypeaheadQuery;
+
+import org.apache.log4j.Logger;
+
+import org.nmdp.hmlfhirconverter.dao.custom.VersionCustomRepository;
+import org.nmdp.hmlfhirconverter.domain.Version;
+import org.nmdp.hmlfhirconverter.dao.VersionRepository;
+
+import org.nmdp.hmlfhirconverter.util.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class VersionServiceImpl implements VersionService {
+    private final VersionRepository versionRepository;
+    private final VersionCustomRepository versionCustomRepository;
+    private static final Logger LOG = Logger.getLogger(VersionServiceImpl.class);
+
+    @Autowired
+    public VersionServiceImpl(@Qualifier("versionRepository") VersionRepository versionRepository,
+                              @Qualifier("versionCustomRepository") VersionCustomRepository versionCustomRepository) {
+        this.versionRepository = versionRepository;
+        this.versionCustomRepository = versionCustomRepository;
+    }
+
+    @Override
+    public Version getVersion(String id) {
+        return versionRepository.findOne(id);
+    }
+
+    @Override
+    public Page<Version> findVersionsByMaxReturn(Integer maxResults, Integer pageNumber) {
+        PageRequest pageable = new PageRequest(pageNumber, maxResults);
+        return versionRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Version> getTypeaheadVersions(Integer maxResults, TypeaheadQuery typeaheadQuery) {
+        Query query = QueryBuilder.buildQuery(maxResults, typeaheadQuery);
+        return versionCustomRepository.findByQuery(query);
+    }
+
+    @Override
+    public List<Version> createVersions(List<io.swagger.model.Version> versions) {
+        List<Version> nmdpModel = versions.stream()
+                .filter(Objects::nonNull)
+                .map(obj -> Version.convertFromSwagger(obj, Version.class))
+                .collect(Collectors.toList());
+
+        return versionRepository.save(nmdpModel);
+    }
+
+    @Override
+    public Version updateVersion(io.swagger.model.Version version) {
+        Version nmdpModel = Version.convertFromSwagger(version, Version.class);
+        return versionRepository.save(nmdpModel);
+    }
+
+    @Override
+    public Boolean deleteVersion(String id) {
+        try {
+            versionRepository.delete(id);
+            return true;
+        } catch (Exception ex) {
+            LOG.error("Error deleting version.", ex);
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean deleteVersion(io.swagger.model.Version version) {
+        try {
+            versionRepository.delete(Version.convertFromSwagger(version, Version.class));
+            return true;
+        } catch (Exception ex) {
+            LOG.error("Error deleting version.", ex);
+            return false;
+        }
+    }
+}
