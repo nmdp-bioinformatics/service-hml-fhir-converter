1cdfc0e6a71c84822283513dfd6398bac2e0337b
Enoch <ekan825@gmail.com>
2017-04-06 13:22:23 -0500

    merge
    Conflicts:
    	src/main/java/org/nmdp/hmlfhirconverter/service/conversion/converters/HmlXmlDeserializer.java

--- src/main/java/org/nmdp/hmlfhirconverter/service/conversion/converters/HmlXmlDeserializer.java
+++ src/main/java/org/nmdp/hmlfhirconverter/service/conversion/converters/HmlXmlDeserializer.java
@@ -69,6 +69,7 @@ import io.swagger.model.Version;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.nmdp.hmlfhirconverter.config.constants.hml.HmlFieldConstants;
+import org.nmdp.hmlfhirconverter.util.PrimitiveParser;
 
 import java.lang.reflect.Type;
 
@@ -570,16 +571,18 @@ public class HmlXmlDeserializer implements JsonDeserializer<Hml> {
             return rawReads;
         }
 
+
+
         rawRead.setId(jsonObject.has(HmlFieldConstants.MONGO_ID) ? jsonObject.get(HmlFieldConstants.MONGO_ID).getAsString() : null);
         rawRead.setActive(jsonObject.has(HmlFieldConstants.MONGO_ACTIVE) ? jsonObject.get(HmlFieldConstants.MONGO_ACTIVE).getAsBoolean() : null);
         rawRead.setDateCreated(jsonObject.has(HmlFieldConstants.MONGO_DATECREATED) ? formatter.parseDateTime(jsonObject.get(HmlFieldConstants.MONGO_DATECREATED).getAsString()).toDate() : null);
         rawRead.setDateUpdated(jsonObject.has(HmlFieldConstants.MONGO_DATEUPDATED) ? formatter.parseDateTime(jsonObject.get(HmlFieldConstants.MONGO_DATEUPDATED).getAsString()).toDate() : null);
-        rawRead.setAdapterTrimmed(jsonObject.has(HmlFieldConstants.RAWREADS_ADAPTERTRIMMED) ? jsonObject.get(HmlFieldConstants.RAWREADS_ADAPTERTRIMMED).getAsBoolean() : null);
+        rawRead.setAdapterTrimmed(PrimitiveParser.parseStringIntegerToBoolean(jsonObject.has(HmlFieldConstants.RAWREADS_ADAPTERTRIMMED) ? jsonObject.get(HmlFieldConstants.RAWREADS_ADAPTERTRIMMED).getAsString() : null));
         rawRead.setAvailability(jsonObject.has(HmlFieldConstants.RAWREADS_AVAILABILITY) ? jsonObject.get(HmlFieldConstants.RAWREADS_AVAILABILITY).getAsString() : null);
         rawRead.setFormat(jsonObject.has(HmlFieldConstants.RAWREADS_FORMAT) ? jsonObject.get(HmlFieldConstants.RAWREADS_FORMAT).getAsString() : null);
-        rawRead.setPaired(jsonObject.has(HmlFieldConstants.RAWREADS_PAIRED) ? jsonObject.get(HmlFieldConstants.RAWREADS_PAIRED).getAsBoolean() : null);
-        rawRead.setPooled(jsonObject.has(HmlFieldConstants.RAWREADS_POOLED) ? jsonObject.get(HmlFieldConstants.RAWREADS_POOLED).getAsBoolean() : null);
-        rawRead.setQualityTrimmed(jsonObject.has(HmlFieldConstants.RAWREADS_QUALITYTRIMMED) ? jsonObject.get(HmlFieldConstants.RAWREADS_QUALITYTRIMMED).getAsBoolean() : null);
+        rawRead.setPaired(PrimitiveParser.parseStringIntegerToBoolean(jsonObject.has(HmlFieldConstants.RAWREADS_PAIRED) ? jsonObject.get(HmlFieldConstants.RAWREADS_PAIRED).getAsString() : null));
+        rawRead.setPooled(PrimitiveParser.parseStringIntegerToBoolean(jsonObject.has(HmlFieldConstants.RAWREADS_POOLED) ? jsonObject.get(HmlFieldConstants.RAWREADS_POOLED).getAsString() : null));
+        rawRead.setQualityTrimmed(PrimitiveParser.parseStringIntegerToBoolean(jsonObject.has(HmlFieldConstants.RAWREADS_QUALITYTRIMMED) ? jsonObject.get(HmlFieldConstants.RAWREADS_QUALITYTRIMMED).getAsString() : null));
         rawRead.setUri(jsonObject.has(HmlFieldConstants.RAWREADS_URI) ? jsonObject.get(HmlFieldConstants.RAWREADS_URI).getAsString() : null);
 
         rawReads.add(rawRead);
