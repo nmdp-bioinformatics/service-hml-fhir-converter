03de61fe31203d3413c470dba9643b703e4357d0
Enoch <ekan825@gmail.com>
2017-04-11 22:46:28 -0500

    merge
    Conflicts:
    	src/main/java/org/nmdp/hmlfhirconverter/service/conversion/converters/HmlXmlDeserializer.java

--- src/main/java/org/nmdp/hmlfhirconverter/config/constants/hml/HmlFieldConstants.java
+++ src/main/java/org/nmdp/hmlfhirconverter/config/constants/hml/HmlFieldConstants.java
@@ -1,27 +1,10 @@
 package org.nmdp.hmlfhirconverter.config.constants.hml;
 
 /**
- * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 3/16/17.
- * <p>
- * service-hmlFhirConverter
- * Copyright (c) 2012-2017 National Marrow Donor Program (NMDP)
- * <p>
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- * <p>
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
- * <p>
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library;  if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
- * <p>
- * > http://www.fsf.org/licensing/licenses/lgpl.html
- * > http://www.opensource.org/licenses/lgpl-license.php
+ * Created by Enoch Kan on 4/6/2017.
+ * HmlFieldConstants.java is a java class written by Enoch Kan to store constants
+ * called in HmlXmlDeserializer.java.  Please refer to HMLGraph_reference.gif for schemas
+ * > https://schemas.nmdp.org/
  */
 
 public class HmlFieldConstants {
@@ -161,7 +144,6 @@ public class HmlFieldConstants {
     public static final String SSP_SCORES = "scores";
 
     //typing test names
-    public static final String TYPINGTESTNAMES_TESTID = "test-id";
     public static final String TYPINGTESTNAMES_NAME = "name";
     public static final String TYPINGTESTNAMES_DESCRIPTION = "description";
 
