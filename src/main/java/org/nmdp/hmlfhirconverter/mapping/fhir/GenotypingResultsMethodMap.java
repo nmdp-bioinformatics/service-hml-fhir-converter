f59f21fc0f229f5e88ce52a17defc504613fa837
Andrew S. Brown, Ph.D <abrown3@NMDP.ORG>
2017-04-25 15:22:05 -0500

    Add GentoypingResults map

---
+++ src/main/java/org/nmdp/hmlfhirconverter/mapping/fhir/GenotypingResultsMethodMap.java
@@ -0,0 +1,58 @@
+package org.nmdp.hmlfhirconverter.mapping.fhir;
+
+/**
+ * Created by Andrew S. Brown, Ph.D., <abrown3@nmdp.org>, on 4/25/17.
+ * <p>
+ * service-hmlFhirConverter
+ * Copyright (c) 2012-2017 National Marrow Donor Program (NMDP)
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library;  if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+ * <p>
+ * > http://www.fsf.org/licensing/licenses/lgpl.html
+ * > http://www.opensource.org/licenses/lgpl-license.php
+ */
+
+import io.swagger.model.Hml;
+import io.swagger.model.Sample;
+import io.swagger.model.Typing;
+import io.swagger.model.TypingMethod;
+import io.swagger.model.SbtNgs;
+
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+import org.nmdp.hmlfhirconverter.domain.fhir.GenotypingResultsMethod;
+
+public class GenotypingResultsMethodMap implements Converter<Hml, GenotypingResultsMethod> {
+
+    @Override
+    public GenotypingResultsMethod convert(MappingContext<Hml, GenotypingResultsMethod> context) {
+        if (context.getSource() == null) {
+            return null;
+        }
+
+        GenotypingResultsMethod genotypingResultsMethod = new GenotypingResultsMethod();
+        Hml hml = context.getSource();
+        Sample sample = hml.getSamples().get(0);
+        Typing typing = sample.getTyping();
+        TypingMethod typingMethod = typing.getTypingMethod();
+        SbtNgs sbtNgs = typingMethod.getSbtNgs();
+
+        genotypingResultsMethod.setTestId(sbtNgs.getTestId());
+        genotypingResultsMethod.setTestIdSource(sbtNgs.getTestIdSource());
+
+        return genotypingResultsMethod;
+    }
+}
